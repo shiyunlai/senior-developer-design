a1be61971fa77d36e548439e976d2857fd9f45eb
165094 <1650943282@qq.com>
2018-06-25 17:56:54 +0800

    svn获取历史记录及获取工作项

--- src/main/java/org/tis/senior/module/developer/service/ISSvnKitService.java
+++ src/main/java/org/tis/senior/module/developer/service/ISSvnKitService.java
@@ -2,9 +2,11 @@ package org.tis.senior.module.developer.service;
 
 
 
+import org.tis.senior.module.developer.entity.vo.SvnCommit;
+
 import java.util.List;
 
 public interface ISSvnKitService {
 
-    
+    List<SvnCommit> loadSvnHistory(String url, Long startRevision);
 }
