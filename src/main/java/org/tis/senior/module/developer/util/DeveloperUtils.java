f49322799867949bbbba6847f32cf5a9622859b2
iiRGB <179494015@qq.com>
2018-06-28 19:40:11 +0800

    开发工具类新增方法

--- src/main/java/org/tis/senior/module/developer/util/DeveloperUtils.java
+++ src/main/java/org/tis/senior/module/developer/util/DeveloperUtils.java
@@ -1,33 +1,49 @@
 package org.tis.senior.module.developer.util;
 
 
+import com.alibaba.fastjson.JSON;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.SVNURL;
+
 public class DeveloperUtils {
 
     /**
      * 获取工程明
+     *
      * @param path
      * @return
      */
-    public static String getProjectName(String path){
+    public static String getProjectName(String path) {
 
         String[] pathSplit = path.split("/");
 
-        if(pathSplit[3].equals("Feature") || pathSplit[3].equals("Hotfix")){
+        if (pathSplit[3].equals("Featrue") || pathSplit[3].equals("Hotfix")) {
 
             return pathSplit[5];
-        }else{
+        } else {
             return pathSplit[4];
         }
     }
 
     /**
      * 获取代码名称
+     *
      * @param svnPath
      * @return
      */
-    public static String getProgramName(String svnPath){
+    public static String getProgramName(String svnPath) {
 
         String[] pathSplit = svnPath.split("/");
-        return pathSplit[pathSplit.length-1];
+        return pathSplit[pathSplit.length - 1];
     }
+
+    /**
+     * 获取工程文件名
+     * @param svnPath
+     * @return
+     */
+    public static String getFilePath(String svnPath)  {
+        return svnPath.substring(svnPath.indexOf(getProjectName(svnPath)));
+    }
+
 }
