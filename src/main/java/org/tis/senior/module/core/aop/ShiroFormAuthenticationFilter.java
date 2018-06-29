dfe3638bf2a889766e3e7224af3ec8e21db599f4
iiRGB <179494015@qq.com>
2018-06-29 16:26:43 +0800

    解决跨域请求被shiro拦截

--- src/main/java/org/tis/senior/module/core/aop/ShiroFormAuthenticationFilter.java
+++ src/main/java/org/tis/senior/module/core/aop/ShiroFormAuthenticationFilter.java
@@ -1,7 +1,6 @@
 package org.tis.senior.module.core.aop;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import org.apache.shiro.web.util.WebUtils;
 
@@ -16,18 +15,19 @@ import javax.servlet.ServletResponse;
  **/
 public class ShiroFormAuthenticationFilter extends FormAuthenticationFilter {
 
-    @Override
-    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-        // 获取当前登录
-        Subject subject = getSubject(request, response);
-        if (subject.getPrincipal() == null) {
-            // 使用response响应流返回数据到前台（因前端需要接受json数据，注意前后端跨域问题）
-            return false;
-        } else {
-            return true;
-        }
-
-    }
+//    @Override
+//    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+//        // 获取当前登录
+//        Subject subject = getSubject(request, response);
+//        if (subject.getPrincipal() == null) {
+//            // 使用response响应流返回数据到前台（因前端需要接受json数据，注意前后端跨域问题）
+//            saveRequestAndRedirectToLogin(request, response);
+//            return false;
+//        } else {
+//            return true;
+//        }
+//
+//    }
 
     @Override
     protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
