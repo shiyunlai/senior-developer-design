40b01711f160a6af1ed1d3016681cc63fa4d9ef6
165094 <1650943282@qq.com>
2018-06-29 21:16:15 +0800

    svn清单申请展示 投放申请和代码投产清单添加 分支版本修改

--- src/main/java/org/tis/senior/module/developer/controller/request/SDliveryAddRequest.java
+++ src/main/java/org/tis/senior/module/developer/controller/request/SDliveryAddRequest.java
@@ -1,14 +1,10 @@
 package org.tis.senior.module.developer.controller.request;
 
-import com.alibaba.fastjson.annotation.JSONField;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.apache.ibatis.annotations.Param;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.NotEmpty;
-import org.tis.senior.module.core.entity.enums.CommonEnumDeserializer;
-import org.tis.senior.module.developer.entity.enums.PackTime;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
 
@@ -16,22 +12,15 @@ import java.util.List;
 public class SDliveryAddRequest {
 
 
-    private String ApplyAlias;
+    @NotBlank(message = "申请别名不能为空")
+    private String applyAlias;
 
-    @NotBlank(message = "工作项的guid不能为空")
-    private Integer guidWorkitem;
-
-    @NotEmpty(message = "投放的运行环境guid不能为空")
-    private List<Integer> guidProfiles;
+    @NotEmpty(message = "投放的运行环境不能为空")
+    private List<DeliveryProfileRequest> deliveryProfileRequest;
 
     @NotBlank(message = "投放说明不能为空")
     private String deliveryDesc;
 
-    @NotBlank(message = "投放窗口不能为空")
-    @JSONField(deserializeUsing = CommonEnumDeserializer.class)
-    private PackTime packTiming;
-
-    @NotBlank(message = "投放时间不能为空")
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @NotNull(message = "投放时间不能为空")
     private Date deliveryTime;
 }
--- src/main/java/org/tis/senior/module/developer/service/impl/SDeliveryListServiceImpl.java
+++ src/main/java/org/tis/senior/module/developer/service/impl/SDeliveryListServiceImpl.java
@@ -5,17 +5,11 @@ import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.tis.senior.module.developer.controller.request.DeliveryListAddRequest;
-import org.tis.senior.module.developer.controller.request.DeliveryListAndDeliveryAddRequest;
-import org.tis.senior.module.developer.controller.request.SDliveryAddRequest;
-import org.tis.senior.module.developer.controller.request.SvnPathListRequest;
-import org.tis.senior.module.developer.entity.SBranch;
-import org.tis.senior.module.developer.entity.SDelivery;
-import org.tis.senior.module.developer.entity.SDeliveryList;
+import org.tis.senior.module.developer.controller.request.*;
+import org.tis.senior.module.developer.entity.*;
 import org.tis.senior.module.developer.dao.SDeliveryListMapper;
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
-import org.tis.senior.module.developer.entity.SProject;
 import org.tis.senior.module.developer.entity.enums.CommitType;
 import org.tis.senior.module.developer.entity.enums.ConfirmStatus;
 import org.tis.senior.module.developer.entity.enums.DeliveryResult;
@@ -24,6 +18,7 @@ import org.tis.senior.module.developer.entity.vo.DeliveryProjectDetail;
 import org.tis.senior.module.developer.entity.vo.SvnCommit;
 import org.tis.senior.module.developer.entity.vo.SvnFile;
 import org.tis.senior.module.developer.entity.vo.SvnPath;
+import org.tis.senior.module.developer.exception.DeveloperException;
 import org.tis.senior.module.developer.service.*;
 import org.springframework.transaction.annotation.Transactional;
 import org.tis.senior.module.developer.util.DeveloperUtils;
@@ -53,7 +48,14 @@ public class SDeliveryListServiceImpl extends ServiceImpl<SDeliveryListMapper, S
     @Autowired
     private ISBranchService branchService;
 
+    @Autowired
     private ISDeliveryService deliveryService;
+
+    @Autowired
+    private ISBranchMappingService branchMappingService;
+
+    @Autowired
+    private ISWorkitemService workitemService;
 
     @Override
     public List<DeliveryProjectDetail> assembleDelivery(String branchGuid) {
@@ -64,6 +66,9 @@ public class SDeliveryListServiceImpl extends ServiceImpl<SDeliveryListMapper, S
                 collect(Collectors.toMap(SProject::getProjectName, p -> p));
 
         List<SvnFile> svnCommits = svnKitService.getDiffStatus(branch.getFullPath(),branch.getCurrVersion().toString());
+        if(svnCommits.size() < 1){
+            throw  new DeveloperException("该清单已被整理或没有最新的提交记录!");
+        }
         List<SDeliveryList> sdList = new ArrayList<>();
         Map<String, List<SvnFile>> commitMap = svnCommits.stream().collect(Collectors.groupingBy(SvnFile::getNodeType));
         Set<String> ecdSet = new HashSet<>();
@@ -136,20 +141,30 @@ public class SDeliveryListServiceImpl extends ServiceImpl<SDeliveryListMapper, S
     @Override
     public void addDeliveryList(DeliveryListAndDeliveryAddRequest request, String userId) throws Exception {
 
+        String guidBranch = request.getGuidBranch();
+        SBranch branch = branchService.selectById(guidBranch);
+        EntityWrapper<SBranchMapping> sbmEntityWrapper = new EntityWrapper<>();
+        sbmEntityWrapper.eq(SBranchMapping.COLUMN_GUID_BRANCH,branch.getGuid());
+        List<SBranchMapping> sbmList = branchMappingService.selectList(sbmEntityWrapper);
+        if(sbmList.size() != 1){
+            throw new Exception("根据分支guid获取的第三方的工作项为空或多条！");
+        }
+        SBranchMapping sbm = sbmList.get(0);
+
         List<SDelivery> deliveryList = new ArrayList<>();
-        List<SDeliveryList> sDeliveryLists = new ArrayList<>();
         SDliveryAddRequest dliveryAddRequest =  request.getDliveryAddRequest();
-        List<Integer> guidPro = dliveryAddRequest.getGuidProfiles();
-        for (int guid:guidPro){
+        List<DeliveryProfileRequest> guidPro = dliveryAddRequest.getDeliveryProfileRequest();
+        for (DeliveryProfileRequest req:guidPro){
             //组装投放申请
             SDelivery delivery = new SDelivery();
-            delivery.setGuidWorkitem(dliveryAddRequest.getGuidWorkitem());
-            delivery.setGuidProfiles(guid);
+            delivery.setApplyAlias(dliveryAddRequest.getApplyAlias());
+            delivery.setGuidWorkitem(sbm.getGuidOfWhats());
+            delivery.setGuidProfiles(Integer.parseInt(req.getGuidProfiles()));
             delivery.setDeliveryType(DeliveryType.GENERAL);
             delivery.setProposer(userId);
             delivery.setApplyTime(new Date());
             delivery.setDeliveryDesc(dliveryAddRequest.getDeliveryDesc());
-            delivery.setPackTiming(dliveryAddRequest.getPackTiming());
+            delivery.setPackTiming(req.getPackTiming());
             delivery.setDeliveryTime(dliveryAddRequest.getDeliveryTime());
             delivery.setDeliveryResult(DeliveryResult.APPLYING);
             deliveryList.add(delivery);
@@ -158,38 +173,16 @@ public class SDeliveryListServiceImpl extends ServiceImpl<SDeliveryListMapper, S
 
 
         for (SDelivery sDelivery:deliveryList){
-            EntityWrapper<SDelivery> deliveryEntityWrapper = new EntityWrapper<>();
-            deliveryEntityWrapper.eq(SDelivery.COLUMN_APPLY_TIME,sDelivery.getApplyTime());
-            deliveryEntityWrapper.eq(SDelivery.COLUMN_GUID_PROFILES,sDelivery.getGuidProfiles());
-            List<SDelivery> sd = deliveryService.selectList(deliveryEntityWrapper);
-            if(sd.size() > 1){
-                throw new Exception("The object of the query does not exist or superfluous!");
-            }
-            SDelivery deli = sd.get(0);
 
             //组装投产代码清单
-            for (DeliveryListAddRequest dlar:request.getDdliveryList()){
-                List<SvnPathListRequest> pathListRequest = dlar.getFileList();
-                for (SvnPathListRequest spr:pathListRequest){
-                    SDeliveryList sdl = new SDeliveryList();
-                    sdl.setGuidDelivery(deli.getGuid());
-                    sdl.setProgramName(spr.getGrogramName());
-                    sdl.setDeliveryVersion(spr.getRevision());
-                    sdl.setPatchType(dlar.getPatchType());
-                    sdl.setDeployWhere(dlar.getProfileWhere());
-                    sdl.setFullPath(spr.getPath());
-                    sdl.setAuthor(spr.getAuthor());
-                    sdl.setCommitDate(spr.getDate());
-                    sdl.setCommitType(spr.getCommitType());
-                    sdl.setDeveloperConfirm(ConfirmStatus.WAIT);
-                    sDeliveryLists.add(sdl);
-                }
+            for (SDeliveryList dlar:request.getDdliveryList()){
+                dlar.setGuid(null);
+                dlar.setGuidDelivery(sDelivery.getGuid());
+                dlar.setDeveloperConfirm(ConfirmStatus.WAIT);
             }
-
+            insertBatch(request.getDdliveryList());
         }
-        insertBatch(sDeliveryLists);
-        String guidBranch = request.getGuidBranch();
-        SBranch branch = branchService.selectById(guidBranch);
+
         int revision = svnKitService.getLastRevision(branch.getFullPath());
         SBranch sb =  new SBranch();
         sb.setCurrVersion(revision);
