ca7e0d2c01c58a1ef6f55f5660423e12e26fca2b
iiRGB <179494015@qq.com>
2018-07-12 09:07:11 +0800

    清单详情

--- src/main/java/org/tis/senior/module/developer/entity/vo/DeliveryProjectDetail.java
+++ src/main/java/org/tis/senior/module/developer/entity/vo/DeliveryProjectDetail.java
@@ -1,6 +1,7 @@
 package org.tis.senior.module.developer.entity.vo;
 
 import lombok.Data;
+import org.tis.senior.module.developer.entity.SCheckList;
 import org.tis.senior.module.developer.entity.SDeliveryList;
 import org.tis.senior.module.developer.entity.SProject;
 
@@ -72,8 +73,6 @@ public class DeliveryProjectDetail {
         return details;
     }
 
-
-
     public static List<DeliveryProjectDetail> getDeliveryDetail(List<SDeliveryList> deliveryLists) {
         List<DeliveryProjectDetail> details = new ArrayList<>();
         deliveryLists.stream()
@@ -83,6 +82,27 @@ public class DeliveryProjectDetail {
                     detail.setProjectName(p);
                     List<DeliveryPatchDetail> dpts = new ArrayList<>();
                     l.stream().collect(Collectors.groupingBy(SDeliveryList::getPatchType)).forEach((pt, list) -> {
+                        DeliveryPatchDetail dpt = new DeliveryPatchDetail();
+                        dpt.setPatchType(pt);
+                        dpt.setDeployWhere(list.get(0).getDeployWhere());
+                        dpt.setFileList(list);
+                        dpts.add(dpt);
+                        detail.setDeliveryPatchDetails(dpts);
+                    });
+                    details.add(detail);
+                });
+        return details;
+    }
+
+    public static List<DeliveryProjectDetail> getCheckDeliveryDetail(List<SCheckList> deliveryLists) {
+        List<DeliveryProjectDetail> details = new ArrayList<>();
+        deliveryLists.stream()
+                .collect(Collectors.groupingBy(SCheckList::getPartOfProject))
+                .forEach((p, l) -> {
+                    DeliveryProjectDetail detail = new DeliveryProjectDetail();
+                    detail.setProjectName(p);
+                    List<DeliveryPatchDetail> dpts = new ArrayList<>();
+                    l.stream().collect(Collectors.groupingBy(SCheckList::getPatchType)).forEach((pt, list) -> {
                         DeliveryPatchDetail dpt = new DeliveryPatchDetail();
                         dpt.setPatchType(pt);
                         dpt.setDeployWhere(list.get(0).getDeployWhere());
