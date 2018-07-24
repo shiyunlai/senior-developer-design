477fbba0eb416d14a3487ec884373de71efdd4fe
165094 <1650943282@qq.com>
2018-07-24 16:49:39 +0800

    :bug:修改投放申请验证和查看投放申请详情

--- src/main/java/org/tis/senior/module/developer/service/ISDeliveryListService.java
+++ src/main/java/org/tis/senior/module/developer/service/ISDeliveryListService.java
@@ -8,6 +8,7 @@ import org.tis.senior.module.developer.entity.SDeliveryList;
 import org.tis.senior.module.developer.entity.vo.DeliveryProjectDetail;
 import org.tmatesoft.svn.core.SVNException;
 
+import java.text.ParseException;
 import java.util.List;
 
 /**
@@ -30,7 +31,7 @@ public interface ISDeliveryListService extends IService<SDeliveryList>  {
      * 添加投放申请和投产代码清单
      * @param request
      */
-    List<SDelivery> addDeliveryList(DeliveryListAndDeliveryAddRequest request, String proposer) throws SVNException;
+    List<SDelivery> addDeliveryList(DeliveryListAndDeliveryAddRequest request, String proposer) throws SVNException, ParseException;
 
     /**
      * 查询此工作项下需要导出成Excel文件的清单代码
